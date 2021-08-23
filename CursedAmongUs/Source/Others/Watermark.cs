8687958b1a3e0c165629cbd9a7846cfa0cba2249
amsyarasyiq <amsyarasyiq@gmail.com>
2021-08-23 17:11:29 +0800

    Added watermark on freeplay

---
+++ CursedAmongUs/Source/Others/Watermark.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CursedAmongUs.Source.Others
+{
+	[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.SetTasks))]
+	public static class CursedWatermark
+	{
+		private const String WatermarkText = 
+			"<size=130%>Cursed Among Us</size>\n" +
+			"Available at github.com/Devs-Us/Cursed-Among-Us\n";
+
+		[HarmonyPostfix]
+		private static void Patch()
+		{
+			PlayerControl player = PlayerControl.LocalPlayer;
+			if (player is null || AmongUsClient.Instance.GameMode != GameModes.FreePlay)
+				return;
+
+			ImportantTextTask watermarkText = 
+				new GameObject("CursedWatermark").AddComponent<ImportantTextTask>();
+
+			watermarkText.transform.SetParent(player.transform, false);
+			watermarkText.Text = $"</color>{WatermarkText}</color>";
+
+			player.myTasks.Insert(0, watermarkText);
+		}
+	}
+}
